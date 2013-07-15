37bfcb51c58798ef8c6352c042cc337ac0bb2ab8
Laura Lindzey <lindzey@gmail.com>
2013-07-15 12:35:45 -0400

    cleaning up code for ex2.57, moving most back to symbolic_math.rkt

--- chapter2/ex2_57.rkt
+++ chapter2/ex2_57.rkt
@@ -1,57 +1,20 @@
 #lang racket
 
-;(require (except-in "symbolic_math.rkt" make-sum addend augend 
-;                    make-product multiplier multiplicand deriv))
-
-
-(define (variable? x) (symbol? x))
-(define (same-variable? v1 v2)
-  (and (variable? v1) (variable? v2) (eq? v1 v2)))
-
-(define (sum? x)
-  (and (pair? x) (eq? (car x) '+)))
-(define (addend s) 
-  (cadr s))
+(require (except-in "symbolic_math.rkt" augend  multiplicand deriv))
+                   
+                    
 (define (augend s) 
   (if (null? (cdddr s))
          (caddr s)
          (cons '+ (cddr s))))
 
-(define (product? x)
-  (and (pair? x) (eq? (car x) '*)))
-(define (multiplier p) (cadr p))
 (define (multiplicand p) 
   (if (null? (cdddr p))
       (caddr p)
       (cons '* (cddr p))))
 
-(define (=number? exp num)
-  (and (number? exp) (= exp num)))
-
-(define (make-sum a1 a2)
-  (cond ((=number? a1 0) a2)
-        ((=number? a2 0) a1)
-        ((and (number? a1) (number? a2)) (+ a1 a2))
-        (else (list '+ a1 a2))))
-
-
-(define (make-product m1 m2)
-  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
-        ((=number? m1 1) m2)
-        ((=number? m2 1) m1)
-        ((and (number? m1) (number? m2)) (* m1 m2))
-        (else (list '* m1 m2))))
-
-(define (make-exponentiation base pow)
-  (cond ((=number? pow 0) 1)
-        ((=number? pow 1) base)
-        (else (list '** base pow))))
-
-(define (exponentiation? x)
-  (and (pair? x) (eq? (car x) '**)))
-(define (base x) (cadr x))
-(define (exponent x) (caddr x))
-
+;; hasn't changed, but need to redefine it here to use the correct versions
+;; of augend and multiplicand
 (define (deriv exp var)
   (cond ((number? exp) 0)
         ((variable? exp)
