efba75f87029f4b91e167f60f4cfdecba074be69
Laura Lindzey <lindzey@gmail.com>
2013-07-15 12:30:20 -0400

    symbolic differentation now works with arbitrary numbers of arguments to + and *

--- chapter2/ex2_57.rkt
+++ chapter2/ex2_57.rkt
@@ -1,20 +1,39 @@
 #lang racket
 
-(require (except-in "symbolic_math.rkt" make-sum addend augend 
-                    make-product multiplier multiplicand deriv))
+;(require (except-in "symbolic_math.rkt" make-sum addend augend 
+;                    make-product multiplier multiplicand deriv))
 
 
-(define (addend s) (cadr s))
-(define (augend s) (caddr s))
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
 
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+(define (addend s) 
+  (cadr s))
+(define (augend s) 
+  (if (null? (cdddr s))
+         (caddr s)
+         (cons '+ (cddr s))))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
 (define (multiplier p) (cadr p))
-(define (multiplicand p) (caddr p))
+(define (multiplicand p) 
+  (if (null? (cdddr p))
+      (caddr p)
+      (cons '* (cddr p))))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
 
 (define (make-sum a1 a2)
   (cond ((=number? a1 0) a2)
         ((=number? a2 0) a1)
         ((and (number? a1) (number? a2)) (+ a1 a2))
         (else (list '+ a1 a2))))
+
 
 (define (make-product m1 m2)
   (cond ((or (=number? m1 0) (=number? m2 0)) 0)
@@ -23,6 +42,15 @@
         ((and (number? m1) (number? m2)) (* m1 m2))
         (else (list '* m1 m2))))
 
+(define (make-exponentiation base pow)
+  (cond ((=number? pow 0) 1)
+        ((=number? pow 1) base)
+        (else (list '** base pow))))
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+(define (base x) (cadr x))
+(define (exponent x) (caddr x))
 
 (define (deriv exp var)
   (cond ((number? exp) 0)
@@ -47,7 +75,31 @@
 
 
 
-(deriv '(+ x 3) 'x)
+(define expr1 '(+ 1 2))
+(eq? 1 (addend expr1))
+(eq? 2(augend expr1))
+(define expr2 '(+ 1 2 3))
+(eq? 1 (addend expr2))
+(eq? 2 (addend (augend expr2)))
+(eq? 3 (augend (augend expr2)))
+
+(eq? 1 (deriv '(+ x 3) 'x))
+(eq? 2(deriv '(+ x 3 x) 'x))
+
+(define expr3 '(* 1 2))
+(eq? 1 (multiplier expr3))
+(eq? 2(multiplicand expr3))
+(define expr4 '(+* 1 2 3))
+(eq? 1 (multiplier expr2))
+(eq? 2 (multiplier (multiplicand expr4)))
+(eq? 3 (multiplicand (multiplicand expr4)))
+
+
+
+;; This works, but isn't simplified (grouping all similar powers of x together ...)
 (deriv '(* x y) 'x)
 (deriv '(* x y (+ x 3)) 'x)
 (deriv '(* (* x y) (+ x 3)) 'x)
+
+
+
